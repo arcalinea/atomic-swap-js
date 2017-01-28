359bd59ee2491373b78ae71a9344dac1f3d35167
Jay Graber <jay@z.cash>
2017-01-28 01:13:05 -0800

    Add

--- atomic-swap.js
+++ atomic-swap.js
@@ -24,10 +24,13 @@ var AtomicSwap = (function(){
       "tx1": tx1,
       "swapScript": swapScript,
       // convert x to hex
-      "x": x.toString('hex')),
+      "x": x.toString('hex'),
       "tx2": tx2
     };
+    return stepOne;
   };
+
+  var FEE = (10**8)*0.0001;
 
   function generateSwapTx(privkeyWif, recipientPubkeyHex, amount, prevOutputs, xHashHex){
     // just generating a privkey here?
@@ -39,13 +42,55 @@ var AtomicSwap = (function(){
     var redeemScript = zcore.Script.fromString(`OP_IF` + ` 2 ${senderPubkey} ${recipientPubkeyHex} 2 OP_CHECKMULTISIGVERIFY ` + `OP_ELSE` + ` ${recipientPubkeyHex} OP_CHECKMULTISIGVERIFY OP_HASH256 ${xHashHex} OP_EQUALVERIFY`); // .raw?
     // Create a pay-to-script hash from the redeemScript
     var p2shScript = redeemScript.toScriptHashOut();
-    // .....
+    // Add a new output with an amount and p2sh script
+    //
+    tx.addOutput()
+
+    // add the inputs. Loop over prevOutputs obj
+    // Where is prevOutputs coming from?
+    var total = 0;
+    for each (var prevOut in prevOutputs){
+      var prevTx = prevOut["tx"]
+      var index = prevOut["index"]
+      total += prevTx.out[index].value;
+      // Add the tx input.....
+      tx.addInput();
+      if (total >= amount + FEE) {break;}
+    };
+    var amount *= (10**8);
+    var change = total - amount - FEE;
+    if (change < 0){throw "Insufficient funds";}
+    tx.addOutput()
+
+    // Sign the inputs.....
+    var inputs = tx.in // Is this the tx inputs arry?
+    for(var i = 0; i < inputs.length; i++){
+      var prevOut = prevOutputs[i];
+      var prevTx = prevOut["tx"];
+      var index = prevOut["index"];
+      // How to derive sigHash?
+      // generate a signature hash for input
+      var sigHash = tx.
+      // index of input in the transaction, add scriptSig
+      // applySignature()
+      try {
+        // verify input signature
+
+      } catch (e) {
+        throw "Signature failure";
+      };
+    };
+
+    // greate new transaction "toPayload"
+    var txToPayload
+    return [txToPayload, redeemScript]
+
   };
 
   function generateRefundTx(privkeyWif, amount, swapTx, swapScript, hours){
     var privkey = new zcore.PrivateKey(privkeyWif).toAddress();
     var refundTx = new Transaction();
-    // .... 
+    // ....
   }
 }());
 
